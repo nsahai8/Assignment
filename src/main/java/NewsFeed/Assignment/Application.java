c397bbc933f0228fbdfbc48e20a827624c919d33
ns8gupta@gmail.com <ns8gupta@gmail.com>
2019-01-01 20:36:34 +0530

    following mvs architecture

--- src/main/java/NewsFeed/Assignment/Application.java
+++ src/main/java/NewsFeed/Assignment/Application.java
@@ -1,18 +1,13 @@
 package NewsFeed.Assignment;
 
-import java.util.Set;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import NewsFeed.Assignment.services.Newspaper;
-import NewsFeed.Assignment.servicesimpl.NewspaperFactory;
-
-@SpringBootApplication(scanBasePackages={"NewsFeed.Assignment"})// same as @Configuration @EnableAutoConfiguration @ComponentScan
+@SpringBootApplication(scanBasePackages = { "NewsFeed.Assignment" })
 public class Application {
- 
-    public static void main(String[] args) {
 
-        SpringApplication.run(Application.class, args);
-    }
+	public static void main(String[] args) {
+
+		SpringApplication.run(Application.class, args);
+	}
 }
--- src/main/java/NewsFeed/Assignment/controllers/AuthorController.java
+++ src/main/java/NewsFeed/Assignment/controllers/AuthorController.java
@@ -2,9 +2,11 @@ package NewsFeed.Assignment.controllers;
 
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import NewsFeed.Assignment.services.AuthorService;
 import NewsFeed.Assignment.services.Newspaper;
 import NewsFeed.Assignment.servicesimpl.NewspaperFactory;
 
@@ -14,14 +16,14 @@ import NewsFeed.Assignment.servicesimpl.NewspaperFactory;
 @RequestMapping(value="/authors")
 public class AuthorController {
 	
-	private Newspaper newspaper;
+	@Autowired
+	private AuthorService authorService;
 	
 //	http://localhost:8080/authors/availableAuthors
 	@RequestMapping(value="/availableAuthors")
 	public Set<String> getAllAvailableAuthors(String newspaperStr){
-		newspaper = NewspaperFactory.getNewspaper("hindu");//Todo
-		Set<String> authors = newspaper.getAuthorLinkMapping().keySet();
-		return authors;
+		//param check and rendor error if any
+		return authorService.getAllAvailableAuthors(newspaperStr);
 		
 	}
 	
